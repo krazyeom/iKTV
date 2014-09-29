822661bf5e13b489c4a1bebaae2cb9e11155c125
Steve Yeom <krazyeom@gmail.com>
2014-09-29 23:32:31 +0900

    Initial Commit

---
+++ iKTV/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iKTV
+//
+//  Created by Steve Yeom on 9/29/14.
+//  Copyright (c) 2014 2nd Jobs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iKTVTests/iKTVTests.m
@@ -0,0 +1,40 @@
+//
+//  iKTVTests.m
+//  iKTVTests
+//
+//  Created by Steve Yeom on 9/29/14.
+//  Copyright (c) 2014 2nd Jobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface iKTVTests : XCTestCase
+
+@end
+
+@implementation iKTVTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
